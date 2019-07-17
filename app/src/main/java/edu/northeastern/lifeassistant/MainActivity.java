92be50d1636d4ad0a540e5e81e4d05b7b2f7ed29
Jacob Piersall <piersall.j@husky.neu.edu>
2019-07-17 17:51:28 -0400

    Crerated Activity List and Event List Screens
    Added the Activity List and Event Lists along with the corresponding adapters, items, and templates

--- app/src/main/java/edu/northeastern/lifeassistant/MainActivity.java
+++ app/src/main/java/edu/northeastern/lifeassistant/MainActivity.java
@@ -2,7 +2,12 @@ package edu.northeastern.lifeassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import util.EventAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changePage(View v) {
+        Intent intent = new Intent(MainActivity.this, ScheduleScreen.class);
+
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/util/ActivityAdapterItem.java
@@ -0,0 +1,19 @@
+package util;
+
+public class ActivityAdapterItem {
+
+    String name;
+    int color;
+    public ActivityAdapterItem(String name, int color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
