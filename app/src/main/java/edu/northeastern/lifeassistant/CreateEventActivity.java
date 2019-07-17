ccfd846f553de85501f4349af3f0fc6749477ac0
Shaurya Carvalho <carvalho.sh@husky.neu.edu>
2019-07-17 14:01:55 -0400

    refactored create event layout

--- app/src/main/java/edu/northeastern/lifeassistant/CreateEventActivity.java
+++ app/src/main/java/edu/northeastern/lifeassistant/CreateEventActivity.java
@@ -2,22 +2,19 @@ package edu.northeastern.lifeassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TimePicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateEventActivity extends AppCompatActivity {
 
     private EditText eventNameEditText;
     private Spinner activitySpinner;
-    private EditText eventStartTimeEditText;
-    private EditText eventEndTimeEditText;
+    private TextView eventStartTimeTextView;
+    private TextView eventEndTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +23,10 @@ public class CreateEventActivity extends AppCompatActivity {
 
         eventNameEditText = findViewById(R.id.eventNameEditText);
         activitySpinner = findViewById(R.id.activitySpinner);
-        eventStartTimeEditText = findViewById(R.id.eventStartTimeEditText);
-        eventEndTimeEditText = findViewById(R.id.eventEndTimeEditText);
+        eventStartTimeTextView = findViewById(R.id.eventStartTimeEditText);
+        eventEndTimeTextView = findViewById(R.id.eventEndTimeEditText);
 
-        eventStartTimeEditText.setOnClickListener(new View.OnClickListener() {
+        eventStartTimeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                TimePickerDialog timePickerDialog = new TimePickerDialog(
@@ -40,9 +37,16 @@ public class CreateEventActivity extends AppCompatActivity {
 //                    }
 //                }, 0, 0, false);
 //                timePickerDialog.show();
-                Toast.makeText(getApplicationContext(),"Hello Javatpoint", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Test", Toast.LENGTH_SHORT).show();
             }
         });
+
+//        eventStartTimeTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View view, boolean b) {
+//                Toast.makeText(getApplicationContext(),"Test", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
