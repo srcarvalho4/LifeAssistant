3d55e86ad31431547fdcb8e94ccb6dbaaee6792a
Shaurya Carvalho <carvalho.sh@husky.neu.edu>
2019-07-16 21:04:36 -0400

    added DayOfWeek picker

--- app/src/main/java/edu/northeastern/lifeassistant/CreateEventActivity.java
+++ app/src/main/java/edu/northeastern/lifeassistant/CreateEventActivity.java
@@ -2,9 +2,15 @@ package edu.northeastern.lifeassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -22,6 +28,21 @@ public class CreateEventActivity extends AppCompatActivity {
         activitySpinner = findViewById(R.id.activitySpinner);
         eventStartTimeEditText = findViewById(R.id.eventStartTimeEditText);
         eventEndTimeEditText = findViewById(R.id.eventEndTimeEditText);
+
+        eventStartTimeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(
+//                        CreateEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+//
+//                    }
+//                }, 0, 0, false);
+//                timePickerDialog.show();
+                Toast.makeText(getApplicationContext(),"Hello Javatpoint", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
